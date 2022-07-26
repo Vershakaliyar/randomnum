97a493f5a95d192f145ed29539506a2297ba8211
vershakaliyar <kaliyarversha@gmail.com>
2022-07-26 20:10:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 
 import './App.css';
 import { useState } from "react";
+import Result from './Result';
+import { render } from '@testing-library/react';
+import {Routes,Route,Link} from 'react-router-dom'
 
 function App() {
   
     const [a1, seta1] = useState(0);
-
+    let a2=a1
+    
+     
     const randNum=() =>{
+   
+     
+    seta1(Math.floor(Math.random(a1)*100))
     if (a1>10) {
 
       window.alert("Target achieved")
-
-
-
+    
     }
-      
-    seta1(Math.floor(Math.random(a1)*100))
      
     }
 
   return (
     <>
+    
     <div class="div1">
-      <h1>{a1}</h1>
+      <Routes>
+
+    <Route exact path='/result' component={Result} />
+    
+    </Routes>
+
+      <p>{a1}</p>
+      
       <button
         type="button"
         onClick={randNum}
       >click me</button>
+      <Result name={a2}/>
+      
+      
       </div>
     </>
   )
---
+++ src/Result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Result=(props) =>{
+return(
+<p>Data form result component-
+{props.name}
+</p>
+
+)
+
+}
+export default Result;
